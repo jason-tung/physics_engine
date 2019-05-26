579fdf5eb748f9ab0d95bfd7153adb4ca5d677c0
Jason Tung <mickeu420@gmail.com>
2019-05-26 00:14:44 -0400

    collision detection works

--- testy.py
+++ testy.py
@@ -7,12 +7,14 @@ hand = Handler()
 
 p1 = Polygon(0, [(0,0),(4,0),(4,4),(0,4)])
 p2 = Polygon(0, [(2,3.5),(-1,9),(2,9),(5,6)])
-p1.o,p2.o = 0,0
+p3 = Polygon(0, [(2,5),(-1,9),(2,9),(5,6)])
+p1.o,p2.o, p3.o = 0,0,0
 
 
 # print("DFSD")
 # line1 = [(0,0),(2,2)]
 # line2 = [(0,2),(2,0)]
-print("test" , utils.line_intersection([(-1,10),(2,3.5)], [(0,0),(4,0)]))
+# print("test" , utils.line_intersection([(-1,10),(2,3.5)], [(0,0),(4,0)]))
 # print("DFSSS")
 print(hand.is_colliding(p1,p2))
+print(hand.is_colliding(p1,p3))
