77b61451d80e3adb97a52ce5ee7da73df22b9ed8
Jason Tung <mickeu420@gmail.com>
2019-06-16 14:55:48 -0400

    gravity

--- dotconn.js
+++ dotconn.js
@@ -9,8 +9,12 @@ var pause = document.getElementById("pause");
 var spec = document.getElementById("spec_inputs");
 var circles = [];
 var pointers = [];
-var mode = "inelastic collisions";
+var apointers = [];
+var mode = "gravity";
 var requestID;
+var mass;
+var g;
+
 var animating, xy, pointer;
 
 var intervalId;
@@ -23,10 +27,10 @@ function getInfo(mode) {
             " and each ball can have an individual mass determined by the input on the right." +
             " Click go to start/resume the simulation." +
             " The simulation can be paused and restarted at any time. " +
-            "A graph on the right will appear after creating your first ball.";
+            "A table on the right will appear after creating your first ball.";
         info.innerHTML = "This simulation is meant to demonstrate the properties of elastic point particles, " +
             "but for the sake of visibility, the points are instead VERY sticky balls!";
-        spec.innerHTML = "mass(kg) <input type=\"number\" class=\"form-control\" id=\"mass\" value=\"20\">";
+        spec.innerHTML = "";
         thead.innerHTML = "<th scope=\"col\">#</th>\n" +
             "                    <th scope=\"col\">x</th>\n" +
             "                    <th scope=\"col\">y</th>\n" +
@@ -40,13 +44,13 @@ function getInfo(mode) {
         instr.innerHTML = "Click and drag to create a ball with an initial velocity vector." +
             " The velocity vector is represented with an arrow," +
             " and each ball can have an individual mass determined by the input on the right." +
+            " There is also an input that will affect the gravitational constant" +
             " Click go to start/resume the simulation." +
             " The simulation can be paused and restarted at any time. " +
-            "A graph on the right will appear after creating your first ball.";
-        info.innerHTML = "This simulation is meant to demonstrate the properties of elastic point particles, " +
-            "but for the sake of visibility, the points are instead VERY sticky balls!";
-        spec.innerHTML = "mass(kg) <input type=\"number\" class=\"form-control\" id=\"mass\" value=\"20\">";
-
+            "A table on the right will appear after creating your first ball. Note that the objects have to be pretty close to demonstrate visible gravity, as this is how gravity works...";
+        info.innerHTML = "This simulation demonstrates gravity between two fragile objects. After two objects collide, they break and disappear into the ether!";
+        spec.innerHTML = "G(10^14 Nm^2/kg^2) <input type=\"number\" class=\"form-control mt-2\" id=\"g\" value=\"5\">";
+        g = document.getElementById("g");
         thead.innerHTML = "<th scope=\"col\">#</th>\n" +
             "                    <th scope=\"col\">x</th>\n" +
             "                    <th scope=\"col\">y</th>\n" +
@@ -56,12 +60,14 @@ function getInfo(mode) {
             "                    <th scope=\"col\">ax</th>\n" +
             "                    <th scope=\"col\">ay</th>"
     }
+    mass = document.getElementById("mass");
 }
 
 
 var dropdown = document.getElementById("dropdown");
-dropdown.addEventListener('change', function (e) {
-    var mode = dropdown.options[dropdown.selectedIndex].value;
+dropdown.addEventListener('change', function () {
+    mode = dropdown.options[dropdown.selectedIndex].value;
+    collision_restart();
     getInfo(mode);
     bighead.innerHTML = mode;
 });
@@ -84,11 +90,15 @@ function updatevar(obj, v, v1, v2, modifier) {
 }
 
 function getloc(circle) {
-    return [circle.getAttribute("cx"), circle.getAttribute("cy")]
+    return [Number(circle.getAttribute("cx")), Number(circle.getAttribute("cy"))]
 }
 
 function getvel(circle) {
-    return [circle.getAttribute("vx"), circle.getAttribute("vy")]
+    return [Number(circle.getAttribute("vx")), Number(circle.getAttribute("vy"))]
+}
+
+function getaccel(circle) {
+    return [Number(circle.getAttribute("ax")), Number(circle.getAttribute("ay"))]
 }
 
 function circle(x, y) {
@@ -96,22 +106,37 @@ function circle(x, y) {
     newcircle.setAttribute('fill', getRandomColor());
     newcircle.setAttribute('cx', x);
     newcircle.setAttribute('cy', y);
-    newcircle.setAttribute('r', 20);
+    newcircle.setAttribute('ax', "0");
+    newcircle.setAttribute('ay', "0");
+    newcircle.setAttribute('r', "20");
     newcircle.setAttribute('stroke', "black");
     // c.appendChild(newcircle);
-    if (mode === "inelastic collisions") {
-        newcircle.setAttribute('mass', mass.value ? mass.value : "10");
-    }
+    newcircle.setAttribute('mass', mass.value ? mass.value : "10");
     return newcircle;
 }
 
-function line(x1, y1, x2, y2) {
+function collision_restart() {
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].remove();
+        pointers[i].remove();
+    }
+    for (i = 0; i < apointers.length; i++) {
+        apointers[i].remove();
+    }
+    circles = [];
+    pointers = [];
+    apointers = [];
+    tbod.innerHTML = "";
+    stopIt();
+}
+
+function line(x1, y1, x2, y2, c) {
     var line = document.createElementNS(svgns, "line");
     line.setAttribute("x1", x1);
     line.setAttribute("y1", y1);
     line.setAttribute("x2", x2);
     line.setAttribute("y2", y2);
-    line.setAttribute("stroke", "black");
+    line.setAttribute("stroke", c);
     line.setAttribute("marker-end", "url(#triangle)");
     line.setAttribute("stroke-width", "3");
     // piline.appendlinehild(line);
@@ -126,9 +151,7 @@ document.onmousemove = function (e) {
 
 c.addEventListener('mousedown', function (e) {
     // console.log("proc");
-    if (mode === "inelastic collisions") {
-        collision_mdown(e);
-    }
+    collision_mdown(e);
 });
 
 c.addEventListener("mouseup", function () {
@@ -150,10 +173,11 @@ var stopIt = function () {
 };
 
 
-go.addEventListener('click', function (e) {
+go.addEventListener('click', function () {
     // console.log("go!");
     if (!animating) {
         animating = true;
+        // console.log(mode);
         updateballs();
     }
 });
@@ -172,7 +196,14 @@ function collision_mdown(e) {
             circles.push(newcircle);
             c.append(newcircle);
             create_pointer(e);
-            console.log(circles[circles.length - 1]);
+            if (mode === "gravity") {
+                create_apointer();
+                for (i = 0; i < circles.length; i++) {
+                    update_gravity(i);
+                }
+                update_apointers();
+            }
+            // console.log(circles[circles.length - 1]);
             chart_append(circles.length - 1);
             if (intervalId) {
                 clearInterval(intervalId);
@@ -183,15 +214,6 @@ function collision_mdown(e) {
     }
 }
 
-function collision_restart() {
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].remove();
-        pointers[i].remove()
-    }
-    circles = [];
-    pointers = [];
-    stopIt()
-}
 
 function update_pointer() {
     // console.log("procing");
@@ -206,6 +228,7 @@ function update_pointer() {
             xy = getloc(circ);
             circ.setAttribute("vx", window.mouseX - xy[0]);
             circ.setAttribute("vy", window.mouseY - xy[1]);
+
         }
     }
     else {
@@ -221,16 +244,41 @@ function update_pointer() {
     }
 }
 
+function update_apointers() {
+    for (var i = 0; i < apointers.length; i++) {
+        pointer = apointers[i];
+        var circle = circles[i];
+        var xy = getloc(circle);
+        pointer.setAttribute("x1", xy[0]);
+        pointer.setAttribute("y1", xy[1]);
+        pointer.setAttribute("x2", Number(xy[0]) + 5 * Number(circle.getAttribute("ax")));
+        pointer.setAttribute("y2", Number(xy[1]) + 5 * Number(circle.getAttribute("ay")));
+    }
+}
+
 function create_pointer(e) {
     var circ = circles[circles.length - 1];
     xy = getloc(circ);
     var cx = xy[0];
     var cy = xy[1];
-    var pointer = line(cx, cy, e.offsetX, e.offsetY);
+    var pointer = line(cx, cy, e.offsetX, e.offsetY, "black");
     pointers.push(pointer);
     c.append(pointer);
     circ.setAttribute("vx", window.mouseX - xy[0]);
     circ.setAttribute("vy", window.mouseY - xy[1]);
+}
+
+function create_apointer() {
+    var circ = circles[circles.length - 1];
+    xy = getloc(circ);
+    var a = getaccel(circ);
+    // console.log(a);
+    var cx = xy[0];
+    var cy = xy[1];
+    var apointer = line(cx, cy, cx + a[0], cy + a[1], "red");
+    // console.log(apointer);
+    apointers.push(apointer);
+    c.append(apointer);
 }
 
 function updateballs() {
@@ -239,10 +287,20 @@ function updateballs() {
         // console.log(circ);
         updatevar(circ, "cx", "cx", "vx", .02);
         updatevar(circ, "cy", "cy", "vy", .02);
-        collision_detect(i);
+        updatevar(circ, "vx", "vx", "ax", 1);
+        updatevar(circ, "vy", "vy", "ay", 1);
+        var gravcol = false;
+        if (mode === "gravity") {
+            update_gravity(i);
+            gravcol = gravity_collision(i);
+        }
+        else {
+            collision_detect(i);
+        }
         chart_update(i);
     }
     update_pointer();
+    update_apointers();
     requestID = window.requestAnimationFrame(updateballs);
 }
 
@@ -252,9 +310,9 @@ function collision_detect(j) {
             // console.log("proc2");
             var xyj = getloc(circles[j]);
             var xyi = getloc(circles[i]);
-            var vxyj = getvel(circles[j]);
-            var vxyi = getvel(circles[i]);
             if (Math.pow(xyi[0] - xyj[0], 2) + Math.pow(xyi[1] - xyj[1], 2) <= 40 * 40) {
+                var vxyj = getvel(circles[j]);
+                var vxyi = getvel(circles[i]);
                 var new_velcx = (vxyi[0] * Number(circles[i].getAttribute("mass")) + vxyj[0] * Number(circles[j].getAttribute("mass"))) / (Number(circles[i].getAttribute("mass")) + Number(circles[j].getAttribute("mass")));
                 // console.log(new_velcx);
                 var new_velcy = (vxyi[1] * Number(circles[i].getAttribute("mass")) + vxyj[1] * Number(circles[j].getAttribute("mass"))) / (Number(circles[i].getAttribute("mass")) + Number(circles[j].getAttribute("mass")));
@@ -269,6 +327,61 @@ function collision_detect(j) {
             }
         }
     }
+}
+
+function rm(i) {
+    circles[i].remove();
+    circles.pop(i);
+    pointers[i].remove();
+    pointers.pop(i);
+    apointers[i].remove();
+    apointers.pop(i);
+
+}
+
+function gravity_collision(i) {
+    for (var j = 0; j < circles.length; j++) {
+        if (i !== j) {
+            // console.log("proc2");
+            var xyj = getloc(circles[j]);
+            var xyi = getloc(circles[i]);
+            if (Math.pow(xyi[0] - xyj[0], 2) + Math.pow(xyi[1] - xyj[1], 2) <= 40 * 40) {
+                circles[i].setAttribute("vx",0);
+                circles[i].setAttribute("vy",0);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getg() {
+    return g.value * Math.pow(10, 14)
+}
+
+function update_gravity(i) {
+    var net_ax = 0;
+    var net_ay = 0;
+    for (var j = 0; j < circles.length; j++) {
+        if (i !== j) {
+            var xyj = getloc(circles[j]);
+            var xyi = getloc(circles[i]);
+            var dx = xyi[0] - xyj[0];
+            var dy = xyi[1] - xyj[1];
+            var theta = Math.atan(dy / dx) + (dx < 0 ? 0 : Math.PI);
+            var d = Math.pow(dx * dx + dy * dy, 2);
+            var magnitude = getg() * circles[i].getAttribute('mass') * circles[j].getAttribute('mass') / (d * d);
+            // console.log(magnitude);
+            var this_ax = magnitude * Math.cos(theta);
+            var this_ay = magnitude * Math.sin(theta);
+            net_ax += this_ax;
+            net_ay += this_ay;
+        }
+    }
+    // console.log(net_ax);
+    // console.log(net_ay);
+    circles[i].setAttribute("ax", net_ax);
+    circles[i].setAttribute("ay", net_ay);
 }
 
 //
@@ -316,26 +429,38 @@ function chart_append(i) {
     var vy = document.createElement("td");
     vy.innerHTML = Number(circle.getAttribute("vy")).toFixed(2);
     vy.setAttribute("id", "vy" + i);
-    var p = document.createElement("td");
-    p.setAttribute("id", "p" + i);
-    p.innerHTML = Number(Math.pow(Math.pow(Number(circle.getAttribute("vx")), 2) + Math.pow(Number(circle.getAttribute("vy")), 2), .5) * Number(circle.getAttribute("mass"))).toFixed(2);
-    var KE = document.createElement("td");
-    KE.innerHTML = Number(Math.pow(Number(circle.getAttribute("vx")), 2) + Math.pow(Number(circle.getAttribute("vy")), 2) / Number(circle.getAttribute("mass"))).toFixed(2);
-    KE.setAttribute("id", "ke" + i);
     tr.appendChild(th);
     tr.appendChild(x);
     tr.appendChild(y);
     tr.appendChild(m);
     tr.appendChild(vx);
     tr.appendChild(vy);
-    tr.appendChild(p);
-    tr.appendChild(KE);
+    if (mode === "inelastic collisions") {
+        var p = document.createElement("td");
+        p.setAttribute("id", "p" + i);
+        p.innerHTML = Number(Math.pow(Math.pow(Number(circle.getAttribute("vx")), 2) + Math.pow(Number(circle.getAttribute("vy")), 2), .5) * Number(circle.getAttribute("mass"))).toFixed(2);
+        var KE = document.createElement("td");
+        KE.innerHTML = Number(Math.pow(Number(circle.getAttribute("vx")), 2) + Math.pow(Number(circle.getAttribute("vy")), 2) / Number(circle.getAttribute("mass"))).toFixed(2);
+        KE.setAttribute("id", "ke" + i);
+        tr.appendChild(p);
+        tr.appendChild(KE);
+    }
+    else {
+        var ax = document.createElement("td");
+        ax.innerHTML = Number(circle.getAttribute("ax")).toFixed(2);
+        ax.setAttribute("id", "ax" + i);
+        var ay = document.createElement("td");
+        ay.innerHTML = Number(circle.getAttribute("ay")).toFixed(2);
+        ay.setAttribute("id", "ay" + i);
+        tr.appendChild(ax);
+        tr.appendChild(ay);
+    }
     tbod.appendChild(tr);
 }
 
 function update_helper(i, type) {
     var circle = circles[i];
-    // console.log(type+i);
+    // console.log(type + i);
     document.getElementById(type + i).innerHTML = Number(circle.getAttribute(type)).toFixed(2);
 }
 
@@ -343,7 +468,13 @@ function chart_update(i) {
     var circle = circles[i];
     circle.setAttribute("p", Number(Math.pow(Math.pow(Number(circle.getAttribute("vx")), 2) + Math.pow(Number(circle.getAttribute("vy")), 2), .5) * Number(circle.getAttribute("mass"))));
     circle.setAttribute("ke", Number(Math.pow(Number(circle.getAttribute("vx")), 2) + Math.pow(Number(circle.getAttribute("vy")), 2) / Number(circle.getAttribute("mass"))));
-    var update_types = ["cx", "cy", "vx", "vy", "p", "ke"];
+    var update_types;
+    if (mode === "inelastic collisions") {
+        update_types = ["cx", "cy", "vx", "vy", "p", "ke"];
+    }
+    else {
+        update_types = ["cx", "cy", "vx", "vy", "ax", "ay"];
+    }
     for (var j = 0; j < update_types.length; j++) {
         update_helper(i, update_types[j]);
     }
